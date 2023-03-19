1a5925b6209050e6c86c60a6088d6ddc7e29e800
Barbora Vesela <barbora.vesela@booking.com>
2023-03-19 19:14:23 +0100

    hotovy ukol

--- index.js
+++ index.js
@@ -1,1 +1,26 @@
 console.log('funguju!');
+
+let number = 1
+const dice = document.querySelector('.dice')
+const throwDice = () => {
+	number = number + 1
+	if (number === 7) {
+		number = 1
+	}
+	dice.src = `img/side${number}.svg`
+}
+document.addEventListener('keydown', throwDice)
+
+/* 
+///// random 
+let number = 1
+const dice = document.querySelector('.dice')
+
+const throwDice = () => {
+    number =  Math.floor(Math.random()*6) + 1 
+    dice.src = `img/side${number}.svg` 
+}
+
+
+document.addEventListener('keydown', throwDice) 
+*/ 
